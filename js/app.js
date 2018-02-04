63261c218ea81325b65a86c3a66c6fcd4a0d5b02
archistico <info@archistico.com>
2018-02-04 19:04:36 +0100

    installazione ambiente test

---
+++ js/app.js
@@ -0,0 +1,7 @@
+class Risolutore {
+
+}
+
+if (typeof module !== 'undefined') {
+    module.exports = Risolutore;
+}
---
+++ js/app.test.js
@@ -0,0 +1,6 @@
+const Risolutore = require('./app');
+
+test('Controllo esecuzione test', () => {
+    let m = 1;
+    expect(m).toEqual(1);
+});
