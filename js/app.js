591e9930c9aeeb1820d2cd37a0713ba49dafc56f
archistico <info@archistico.com>
2018-02-04 20:52:55 +0100

    separazione e test ok

--- js/app.js
+++ js/app.js
@@ -1,7 +1,66 @@
-class Risolutore {
+function calcola(DistanzaCm, LatoOri, LatoVer, Abbondanza, OpzioneAcuita) {
 
+    let SecToRad = 0.000004848136811;
+    let DistanzaInch = 0;
+    let PPI = 0;
+    let DistanzaConsigliata = 0;
+    let PixelOri = 0;
+    let PixelVer = 0;
+    let Acuita = 0;
+
+    switch (OpzioneAcuita)
+    {
+        case 0:
+            Acuita = SecToRad * 30;
+            break;
+        case 1:
+            Acuita = SecToRad * 35;
+            break;
+        case 2:
+            Acuita = SecToRad * 40;
+            break;
+        case 3:
+            Acuita = SecToRad * 45;
+            break;
+        case 4:
+            Acuita = SecToRad * 50;
+            break;
+        case 5:
+            Acuita = SecToRad * 55;
+            break;
+        case 6:
+            Acuita = SecToRad * 60;
+            break;
+    }
+
+    DistanzaInch = DistanzaCm / 2.54;
+    PPI = 1 / (2 * DistanzaInch * Math.tan(Acuita / 2));
+
+    let LatoMaggiore = 0;
+    if (LatoOri >= LatoVer)
+        LatoMaggiore = LatoOri + 2 * Abbondanza;
+    else
+        LatoMaggiore = LatoVer + 2 * Abbondanza;
+
+    let angoloConoOtticoGra = 60;
+    let angoloConoOtticoRad = angoloConoOtticoGra * Math.PI / 180;
+
+    DistanzaConsigliata = LatoMaggiore * Math.cos(angoloConoOtticoRad / 2);
+
+    if (PPI != 0)
+    {
+        PixelOri = LatoOri / 2.54 * PPI;
+        PixelVer = LatoVer / 2.54 * PPI;
+    }
+
+    return {
+        "PPI": PPI.toFixed(0),
+        "DISTANZACONSIGLIATA": DistanzaConsigliata.toFixed(0),
+        "PIXELORIZZONTALI": PixelOri.toFixed(0),
+        "PIXELVERTICALI": PixelVer.toFixed(0)
+    };
 }
 
 if (typeof module !== 'undefined') {
-    module.exports = Risolutore;
+    module.exports = calcola;
 }
--- js/app.test.js
+++ js/app.test.js
@@ -1,6 +1,11 @@
-const Risolutore = require('./app');
+const calcola = require('./app');
 
-test('Controllo esecuzione test', () => {
-    let m = 1;
-    expect(m).toEqual(1);
+test('Verifica 1', () => {
+    let risultato = calcola(250, 300, 200, 3, 0);
+    expect(risultato).toEqual({PPI: "70", DISTANZACONSIGLIATA: "265", PIXELORIZZONTALI: "8251", PIXELVERTICALI: "5500"});
+});
+
+test('Verifica 2', () => {
+    let risultato = calcola(250, 100, 200, 3, 6);
+    expect(risultato).toEqual({PPI: "35", DISTANZACONSIGLIATA: "178", PIXELORIZZONTALI: "1375", PIXELVERTICALI: "2750"});
 });
